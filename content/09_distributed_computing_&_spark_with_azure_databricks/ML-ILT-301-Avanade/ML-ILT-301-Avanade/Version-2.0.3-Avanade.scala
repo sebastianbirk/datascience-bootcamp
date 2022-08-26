e09b2b0493e5fb485b7e4a1781c06f66d2c8a1ba
Sebastian Birk <sebastian.birk@outlook.com>
2022-08-26 07:22:58 +0200

    Delete content/09_distributed_computing_&_spark_with_azure_databricks/ML-ILT-301-Avanade directory

--- content/09_distributed_computing_&_spark_with_azure_databricks/ML-ILT-301-Avanade/ML-ILT-301-Avanade/Version-2.0.3-Avanade.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: ML-ILT-301R
-// MAGIC * Version 2.0.3
-// MAGIC * Built 2019-11-22 16:47:32 UTC
-// MAGIC * Git revision: b4fb490975ded4a3494fe07861d6d4a550c20a4e
-// MAGIC 
-// MAGIC Copyright © 2019 Databricks, Inc.
